3d30c95a6ecb1880ecea307f14456f74498b8c77
KirillLutsenko <mnrovski68@gmail.com>
2020-10-22 18:56:22 +0300

    Updates

---
+++ precache-manifest.3555fa711d22f2fce1b7dae34ba05c91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905dd67faa08349911510f68c1b20c8b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b5c11413e7b4c9b708f",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "8e5f7d8441eb2cd51bc5",
+    "url": "./static/css/main.b0cfdfd3.chunk.css"
+  },
+  {
+    "revision": "2b5c11413e7b4c9b708f",
+    "url": "./static/js/2.77fe2da9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.77fe2da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e5f7d8441eb2cd51bc5",
+    "url": "./static/js/main.e239a7c3.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.3555fa711d22f2fce1b7dae34ba05c91.js"
 );
 
 self.addEventListener('message', (event) => {
